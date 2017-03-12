db5024445fa69b4adaeb9856fd78790a8253baa3
Michael Corcoran <tracer@outlook.co.nz>
2017-03-12 15:03:07 +1300

    Work around lack of plugin support on gh-pages
    Plus a couple of other small tweaks

--- assets/js/tunes.js
+++ assets/js/tunes.js
@@ -6,7 +6,7 @@ function autotown_api(path) {
 
 function download(filename, text) {
     var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('href', 'data:application/xml;charset=utf-8,' + encodeURIComponent(text));
     element.setAttribute('download', filename);
     element.style.display = 'none';
     document.body.appendChild(element);
